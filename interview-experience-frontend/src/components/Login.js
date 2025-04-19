436dd648fed763d5ffcab4bbc28ad34e18cb41eb
Anshika Panwar <anshikapanwar74@gmail.com>
2025-04-19 18:39:09 +0530

    Update Login.js

--- interview-experience-frontend/src/components/Login.js
+++ interview-experience-frontend/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const BASE_URL = "https://inter-experience.onrender.com";
+const BASE_URL = "https://inter-experience.onrender.com"
 
 const Login = () => {
   const [email, setEmail] = useState('');
